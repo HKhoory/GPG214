5efda65769c8b5abcf7cca26c6ae96719ddd388e
NullPerson <edu1khoory@hotmail.com>
2024-11-03 23:30:30 +0400

    More changes

--- GPG214P1/Assets/2DGamekit/Scripts/LoadingAssets/TextureLoader.cs
+++ GPG214P1/Assets/2DGamekit/Scripts/LoadingAssets/TextureLoader.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class TextureLoader : MonoBehaviour
 {
+
+    [SerializeField] private GameObject spriteTexture;
+    //[SerializeField] private AudioClip audioClip;
+    [SerializeField] private string textureFolder;
+    [SerializeField] private string textureName;
+    //[SerializeField] private string soundFolder;
+    private string folderPath = Application.streamingAssetsPath;
+
+
+    private void Awake()
+    {
+        if (spriteTexture == null)
+        {
+            spriteTexture = Resources.Load<GameObject>("TestTexture");
+        }
+
+        //if (spriteTexture != null)
+        //{
+        //    Instantiate(spriteTexture);
+        //}
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +37,23 @@ public class TextureLoader : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void LoadTexture()
+    {
+        if (File.Exists(folderPath))
+        {
+            byte[] imageBytes = File.ReadAllBytes(folderPath + "/" + textureFolder + "/" + textureName);
+
+            Texture2D texture = new Texture2D(2, 2);
+
+            texture.LoadImage(imageBytes);
+
+            spriteTexture.GetComponent<Renderer>().material.mainTexture = texture;
+        }
+        else
+        {
+            Debug.Log("Texture file not found at" + folderPath);
+        }
     }
 }
